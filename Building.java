c7b18e8c28c5a246ef1faa451d160027b5f3ea3d
estavick <estavick@yahoo.com>
2016-09-26 17:07:39 -0700

    updated three classes

--- Building.java
+++ Building.java
@@ -1,10 +1,15 @@
-//***********************
-//Building.java				Author: Lewis/Loftus
-//
-//Represents a building with a particular width, height, and color.
-//*************************
-
 import java.awt.*;
+
+/*****
+ * @authors Wanzin Yazar and Edward Stavick 
+ * @version 1.0
+ * @since September 26, 2016
+ * @param upperX, upperY, shade, width, and height ---- to initialize
+ * @args w, h, color, x, and y for the location, dimensions, and color of the building.
+ * @exception Any exception.
+ * @return w, h, color, x, and y for the location, dimensions, and color of the building.
+*****/
+
 public class Building
 {
     private int w, h, x, y;
@@ -14,86 +19,87 @@ public class Building
     //  Constructor: Sets up this circle with the specified values.
     //-------------------------
     
-    public Building ( int upperX, int upperY, int width, int height, Color shade)
+    public Building (int upperX, int upperY, Color shade, int width, int height)
     {
-    	color    = shade;
-    	w    	 = width;
-    	h        = height;
-    	x        = upperX;
-    	y 		 = upperY;
+    	color = shade;
+    	w = width;
+    	h = height;
+    	x = upperX;
+    	y = upperY;
     }
     
     //--------------------------------
-    // Draws this rectangle in the specified graphic context.
+    // Draws this building in the specified graphics context.
     //---------------------------------
     
     public void draw (Graphics page)
     {
-    	page.setColor (color);
-    	page.fillRect (x, y, w, h);
-    	page.setColor (color.blue);
-    	page.fillRect (x+3, y+3, 5, 5);
+    	page.setColor(color);
+    	page.fillRect(x, y, w, h);
+
+    	//Draws the windows on the buildings.
+    	page.setColor(color.blue);
+    	page.fillRect(x+3, y+3, 5, 5);
+    	page.fillRect(x+12, y+3, 5, 5);
     }
     
-	//---------------------
-	//Rectangle mutator.
-	//---------------------
-	
+	//Width mutator.
 	public void setW (int width)
 	{
 		w = width;
 	}
 	
+	//Height mutator.
 	public void setH (int height)
 	{
 		h = height;
 	}
 	
-	//--------------------
 	//Color mutator.
-	//-------------------
-	
 	public void setColor (Color shade)
 	{
 		color = shade;
 	}
 	
-	//-----------------------
-	
+	//X mutator.
 	public void setX  (int upperX)
 	{
 		x = upperX;
 	}
 	
+	//Y mutator.
 	public void setY  (int upperY)
 	{
 		y = upperY;
 	}
-	
-	//------------------------
-	// rectangle accessor
-	//------------------------
-	
+
+	//Width accessor.
 	public int getW ()
 	{
 		return w;
 	}
 	
+	//Height accessor.
 	public int getH ()
 	{
 		return h;
 	}
 	
+	//Color accessor.
 	public Color getColor ()
 	{
 		return color;
 	}
 	
+	
+	// X accessor.
 	public int getX ()
 	{
 		return x;
 	}
 	
+	
+	// Y accessor.
 	public int getY ()
 	{
 		return y;
--- SkylinePanel.java
+++ SkylinePanel.java
@@ -1,42 +1,50 @@
-//********************************************************************
-//  SplatPanel.java       Author: Lewis/Loftus
-//
-//  Demonstrates the use of graphical objects.
-//********************************************************************
-
 import javax.swing.*;
 import java.awt.*;
 
+/*****
+ * @authors Wanzin Yazar and Edward Stavick 
+ * @version 1.0
+ * @since September 26, 2016
+ * @
+ * @
+ * @exception Any exception.
+*****/
+
 public class SkylinePanel extends JPanel
 {
-   private Building rectangle1, rectangle2, rectangle3, rectangle4, rectangle5;
+   private Building building1, building2, building3, building4, building5;
 
    //-----------------------------------------------------------------
-   //  Constructor: Creates five Circle objects.
+   //  Constructor: Creates five building objects.
    //-----------------------------------------------------------------
    public SkylinePanel()
    {
-      rectangle1 = new Rectangle (0, 100, Color.red, 10, 120);
-      rectangle2 = new Rectangle (20, 60, Color.green, 40, 120);
-      rectangle3 = new Rectangle (50, 70, Color.cyan, 60, 120);
-      rectangle4 = new Rectangle (70, 65, Color.yellow, 80, 120);
-      rectangle5 = new Rectangle (100, 85, Color.blue, 110, 120);
-
+      building1 = new Building (5, 150, Color.black, 45, 50);
+      building2 = new Building (70, 85, Color.green, 30, 115);
+      building3 = new Building (120, 120, Color.red, 60, 80);
+      building4 = new Building (195, 100, Color.gray, 25, 100);
+      building5 = new Building (235, 140, Color.white, 20, 60);
+      
+      //Sets the resolution and color of the panel.
       setPreferredSize (new Dimension(300, 200));
-      setBackground (Color.black);
+      setBackground (Color.pink);
+      
+      //Draws the name of the city.
+      JLabel cityName = new JLabel("Wanzin and Edward's City");
+      this.add(cityName);
    }
 
    //-----------------------------------------------------------------
-   //  Draws this panel by requesting that each circle draw itself.
+   //  Draws this panel by requesting that each building draw itself.
    //-----------------------------------------------------------------
    public void paintComponent (Graphics page)
    {
       super.paintComponent(page);
 
-      rectangle1.draw(page);
-      rectangle2.draw(page);
-      rectangle3.draw(page);
-      rectangle4.draw(page);
-      rectangle5.draw(page);
+      building1.draw(page);
+      building2.draw(page);
+      building3.draw(page);
+      building4.draw(page);
+      building5.draw(page);
    }
 }
